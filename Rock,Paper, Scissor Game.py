2815ff46455688fce2da9aca2194126db9a541ce
sakaria <sakaria@cisco.com>
2021-09-28 11:27:17 -0700

    Few Changes

--- Rock,Paper, Scissor Game.py
+++ Rock,Paper, Scissor Game.py
@@ -1,19 +1,12 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
-#!/bin/python3
 
 from random import randint 
+
 name=input('What is your name?')
 game=input('Hey! Choose any one from Rock(r),Paper(p),Scissors(s) :')
-print(game ,'vs', end=' ')
+print(game ,'vs',end=' ')
 choosen= randint(1,3)
-#print(choosen)
+
 if choosen==1:
   computer='r'
   
@@ -33,18 +26,19 @@ elif game == 'r' and computer == 'p':
   print('Computer Wins!')
   
 elif game == 'r' and computer == 's':
-  print(name ,'Wins!')
+  print( name , 'Wins!')
+  
   
 elif game == 's' and computer == 'p':
   print('Computer Wins!')
   
 elif game == 's' and computer == 'r':
-  print(name, 'Wins!')
+  print( name , 'Wins!')
   
 elif game == 'p' and computer == 's':
   print('Computer Wins!')
   
 elif game == 'p' and computer == 'r':
-  print(name, 'Wins!')
+  print( name , 'Wins!')
   
 
